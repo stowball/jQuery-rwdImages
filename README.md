de71aa373ee2003da02312a7e02579813b4c7957
Matt Stow <matt@mattstow.com>
2013-01-21 19:40:53 +1100

    Added options, performance enhancements

--- README.md
+++ README.md
@@ -3,8 +3,26 @@ jQuery RWD Images
 
 ### Allows responsive content images using the redux spacer technique (http://mattstow.com/experiment/responsive-images-redux/responsive-images-redux-jquery-plugin.html) to be shared and saved
 
+The plugin has 2 default options which can be overwritten as required:
+
+```
+'display': 'inline-block' // Sets the display property of the images' wrapping .rwd_wrap span
+'zindex': false // Sets the z-index property of the images' wrapping .rwd_wrap span
+```
+
 * * *
 
 Usage:
 
-$('img').rwdImages();
+Basic
+
+`$('img').rwdImages();`
+
+Overriding default options
+
+```
+$('img').rwdImages({
+	'display': 'block',
+	'zindex:' 1
+});
+```
--- jquery.rwdImages.js
+++ jquery.rwdImages.js
@@ -1,5 +1,5 @@
 /*
-* rwdImages jQuery plugin v1.1
+* rwdImages jQuery plugin v1.2
 *
 * Allows responsive content images using the redux spacer technique (http://mattstow.com/experiment/responsive-images-redux/responsive-images-redux-jquery-plugin.html) to be shared and saved
 *
@@ -8,19 +8,37 @@
 * Licensed under the MIT license
 */
 ;(function($) {
-	$.fn.rwdImages = function() {
+	$.fn.rwdImages = function(options) {
+		var settings = $.extend({
+			display: 'inline-block',
+			zindex: false
+		}, options);
+		
 		var $that = $(this),
-			ltie9 = $.browser.msie && parseInt($.browser.version) <= 8 ? true : false;
+			ltie9 = $.browser.msie && parseInt($.browser.version) <= 8 ? true : false,
+			css = 'display: ' + settings.display + '; max-width: 100%; position: relative;';
+			
+		if (settings.zindex)
+			css += ' z-index: ' + settings.zindex + ';';
 			
 		if ($that.length > 0 && !ltie9) {
 			var backgroundImage = 'background-image',
 				src = /(url\("?)(.*?)("?\))/gi,
 				attrSrc = 'src',
 				swap = 'rwd-swap';
+				
 			$that.each(function() {
-				var $this = $(this);
+				var $this = $(this),
+					newSrc,
+					bg = $this.css(backgroundImage);
+					
+				if (bg.match(src))
+					newSrc = bg.replace(src, '$2');
+				else
+					newSrc = '#';
+
 				$this
-					.wrap('<span style="display: inline-block; max-width: 100%; position: relative;" />')
+					.wrap('<span class="rwd-wrap" style="' + css + '" />')
 					.clone()
 					.removeClass($that.attr('class'))
 					.addClass(swap)
@@ -33,12 +51,20 @@
 						width: '100%'
 					})
 					.appendTo($this.parent())
-					.attr(attrSrc, $this.css(backgroundImage).replace(src, '$2'));
+					.attr(attrSrc, newSrc);
 			});
 			$(window).resize(function() {
 				$('img.' + swap).each(function() {
-					var $this = $(this);
-					$this.attr(attrSrc, $this.prev().css(backgroundImage).replace(src, '$2'));
+					var $this = $(this),
+					newSrc,
+					bg = $this.css(backgroundImage);
+					
+					if (bg.match(src))
+						newSrc = bg.replace(src, '$2');
+					else
+						newSrc = '#';
+
+					$this.attr(attrSrc, newSrc);
 				});
 			});
 		}
