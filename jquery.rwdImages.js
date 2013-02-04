5da3920484ef63778c889605ab3883e41058d766
Matt Stow <matt@mattstow.com>
2013-02-04 16:42:15 +1100

    Fixed swap image on resize. Feature detected lt IE 9. Performance improvements

--- jquery.rwdImages.js
+++ jquery.rwdImages.js
@@ -1,9 +1,9 @@
 /*
-* rwdImages jQuery plugin v1.2
+* rwdImages jQuery plugin v1.3
 *
 * Allows responsive content images using the redux spacer technique (http://mattstow.com/experiment/responsive-images-redux/responsive-images-redux-jquery-plugin.html) to be shared and saved
 *
-* Copyright (c) 2012 Matt Stow
+* Copyright (c) 2013 Matt Stow
 * http://mattstow.com
 * Licensed under the MIT license
 */
@@ -15,18 +15,31 @@
 		}, options);
 		
 		var $that = $(this),
-			ltie9 = $.browser.msie && parseInt($.browser.version) <= 8 ? true : false,
+			style = document.documentElement.style,
+			ltie9 = 'behavior' in style && 'widows' in style && !'fill' in style ? true : false,
 			css = 'display: ' + settings.display + '; max-width: 100%; position: relative;';
-			
-		if (settings.zindex)
-			css += ' z-index: ' + settings.zindex + ';';
 			
 		if ($that.length > 0 && !ltie9) {
 			var backgroundImage = 'background-image',
 				src = /(url\("?)(.*?)("?\))/gi,
 				attrSrc = 'src',
-				swap = 'rwd-swap';
-				
+				swap = 'rwd-swap',
+				$imgs;
+			
+			if (settings.zindex)
+				css += ' z-index: ' + settings.zindex;
+			
+			$('head').append(
+				'<style>' +
+					'.' + swap + '{' +
+					'height: 100%;' +
+					'left: 0;' +
+					'opacity: 0;' +
+					'position: absolute;' +
+					'top: 0;' +
+					'width: 100%}' +
+				'</style>');
+			
 			$that.each(function() {
 				var $this = $(this),
 					newSrc,
@@ -34,37 +47,27 @@
 					
 				if (bg.match(src))
 					newSrc = bg.replace(src, '$2');
-				else
-					newSrc = '#';
 
 				$this
 					.wrap('<span class="rwd-wrap" style="' + css + '" />')
 					.clone()
 					.removeClass($that.attr('class'))
 					.addClass(swap)
-					.css({
-						height: '100%',
-						left: 0,
-						opacity: 0,
-						position: 'absolute',
-						top: 0,
-						width: '100%'
-					})
 					.appendTo($this.parent())
 					.attr(attrSrc, newSrc);
 			});
+			
+			$imgs = $('img.' + swap);
+			
 			$(window).resize(function() {
-				$('img.' + swap).each(function() {
+				$imgs.each(function() {
 					var $this = $(this),
-					newSrc,
-					bg = $this.css(backgroundImage);
+						$prev = $this.prev(),
+						newSrc,
+						bg = $prev.css(backgroundImage);
 					
 					if (bg.match(src))
-						newSrc = bg.replace(src, '$2');
-					else
-						newSrc = '#';
-
-					$this.attr(attrSrc, newSrc);
+						$this.attr(attrSrc, bg.replace(src, '$2'));
 				});
 			});
 		}
